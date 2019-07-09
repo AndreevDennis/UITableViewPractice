92979fa8be59d120d5a46b3ef66eb7b51183a044
DennisAndreev <denisandreev1611@gmail.com>
2019-07-09 20:23:03 +0300

    Initial Commit

---
+++ UITableViewPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITableViewPractice
+//
+//  Created by Денис Андреев on 7/9/19.
+//  Copyright © 2019 Денис Андреев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
