2b702a2a8ca72aeb20b030e581b0ceb3970016ac
DennisAndreev <denisandreev1611@gmail.com>
2019-07-09 20:59:14 +0300

    table with code

--- UITableViewPractice/ViewController.swift
+++ UITableViewPractice/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+   var myTableView = UITableView()
+    var ident = "MyCell"
 
+    
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       self.myTableView = UITableView(frame: view.bounds, style: .plain)
+        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: ident)
+        myTableView.delegate = self
+        myTableView.dataSource = self
+        
+        myTableView.autoresizingMask = [.flexibleWidth, .flexibleWidth]
+        view.addSubview(myTableView)
+
     }
 
 
+
+
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ident,for: indexPath)
+        cell.textLabel?.text = "row = \(indexPath.row)"
+        cell.backgroundColor = .blue
+        return cell
+    }
+    
+    
 }
 
